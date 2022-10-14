277d1996af2e9c8939e980c14f859f8b57a45417
Matt <21099MHR@gmail.com>
2022-10-14 19:50:35 -0400

    Current solutions for the Calculator Project.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,35 +1,83 @@
 class Calculator {
-    firstNumber;
-    secondNumber;
-    operator;
-    value;
+  firstNumber;
+  secondNumber;
+  operator;
+  value;
 
-    constructor(firstNumber, secondNumber, operator) {
-        this.firstNumber = firstNumber;
-        this.secondNumber = secondNumber;
-        this.operator = operator;
-        this.value = 0;
+  constructor(firstNumber, secondNumber, operator) {
+    this.firstNumber = firstNumber;
+    this.secondNumber = secondNumber;
+    this.operator = operator;
+    this.value = 0;
+  }
+
+  // determine the current operation's label
+  getAction() {
+    var operator = getOperator();
+
+    switch (operator) {
+      case "add":
+        return "add";
+        break;
+      case "subtract":
+        return "subtract";
+        break;
+      case "multiply":
+        return "multiply";
+        break;
+      case "divide":
+        return "divide";
+        break;
+      default:
+        alert("...how?");
+        break;
+    }
+  }
+
+  // perform a calculation based on the currently selected operation
+  operate(operator) {
+    switch (operator) {
+      case "add":
+        this.add();
+        break;
+      case "subtract":
+        this.subtract();
+        break;
+      case "multiply":
+        this.multiply();
+        break;
+      case "divide":
+        this.divide();
+        break;
     }
 
-    // determine the current operation's label
-    getAction() {}
+    return this.value;
+  }
 
-    // performa a calculation based on the currently selected operation
-    operate() {}
+  attemptedDivideByZero = false;
 
-    attemptedDivideByZero = false;
+  // perform addition operation
+  add() {
+    this.value = this.firstNumber + this.secondNumber;
+  }
 
-    // perform addition operation
-    add() {}
+  // perform subtraction operation
+  subtract() {
+    value = this.firstNumber - this.secondNumber;
+  }
 
-    // perform subtraction operation
-    subtract() {}
+  // perform multiplication operation
+  multiply() {
+    this.value = this.firstNumber * this.secondNumber;
+  }
 
-    // perform multiplication operation
-    multiply() {}
-
-    // perform divide operation
-    divide() {}
+  // perform divide operation
+  divide() {
+    if (this.secondNumber == 0) {
+      this.attemptedDivideByZero = true;
+      alert("Attempting to divide by 0. Operation aborted.");
+    } else this.value = this.firstNumber / this.secondNumber;
+  }
 }
 
 //DO NOT TOUCH THIS LINE OF CODE//
--- calculator/controller.js
+++ calculator/controller.js
@@ -1,20 +1,41 @@
 // setup variables and get a calculator instance
-function CreateCalculator() {}
+function CreateCalculator() {
+  const calculator = new Calculator();
+  calculator.firstNumber = document.getElementById("number_one").value;
+  calculator.secondNumber = document.getElementById("number_two").value;
+  return calculator;
+}
 
 // perform a calculation when the operator button is clicked
-function calculate() {}
+function calculate() {
+    var calculator = CreateCalculator();
+    var operator = getOperator();
+    var value = calculator.operate(operator);
+    updateResultText(value);
+}
 
 /**
  * set the text in the result section of the UI
  * @param {*} value
  */
-function updateResultText(value) {}
+function updateResultText(value) {
+  document.getElementById("result_text").innerHTML = String(value);
+}
 
 // should clear input text values and focus the first number input
-function clearValues() {}
+function clearValues() {
+  document.getElementById("number_one").value = "";
+  document.getElementById("number_two").value = "";
+  document.getElementById("result_text").innerHTML =
+    "Values have been cleared.";
+  document.getElementById("number_one").focus();
+}
 
 /**
  * get the selected operator from the UI
  * @returns Operators
  */
-function getOperator() {}
+function getOperator() {
+  var operator = document.querySelector('input[name="math"]:checked').value;
+  return operator;
+}
